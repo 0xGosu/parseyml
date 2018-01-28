ed2a833175d6899e3c7095f2cb2e7eac33ef33b0
V.Anh Tran <tranvietanh1991@gmail.com>
2018-01-28 17:45:59 +0800

    fix build support python 2.6

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     install_requires=[
         "PyYAML>=3.11",
     ],
+    python_requires='>=2.6, <3',
     keywords=['yaml', 'parse', 'parseyml', 'parse-yml', 'yml', 'export', 'shell'],  # arbitrary keywords
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -37,6 +38,8 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
         # "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
